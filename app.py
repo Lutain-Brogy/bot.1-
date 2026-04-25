8f3d410eba70510dfaf3d80f78c10f4262db7a88
Lutain-Brogy <brogylutain@gmail.com>
2026-04-25 07:37:33 +0200

    Update app.py
    Updated.1

--- app.py
+++ app.py
@@ -5,4 +5,16 @@ st.title("My AI Chatbot UI")
 user_input = st.text_input("You:")
 
 if user_input:
-    st.write("Bot: You said ->", user_input)
+
+    # simple if/else brain
+    if user_input.lower() == "hello":
+        st.write("Bot: Hello! 👋")
+
+    elif user_input.lower() == "how are you":
+        st.write("Bot: I'm good! How can I help you?")
+
+    elif user_input.lower() == "bye":
+        st.write("Bot: Goodbye! 👋")
+
+    else:
+        st.write("Bot: I don't understand that yet.")
