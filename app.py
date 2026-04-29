7b5ce97e4fc55fc55a85c4edffb7c57ffe10e2cc
Lutain-Brogy <brogylutain@gmail.com>
2026-04-30 00:21:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.title("AI Business Assistant")
 
 user_input = st.text_input("You:")
-text = "Hi there, I have a new case"
-if user_input:
-clean_text = text.lower()
 
-if "new case" in clean_text:
-    print("Cool, proceed by stating your case.")
-else:
-    print("Hello how can I help?")
+if user_input:
+    clean_text = user_input.lower()
+
+    if "new case" in clean_text:
+        st.write("Cool, proceed by stating your case.")
+    else:
+        st.write("Hello how can I help?")
