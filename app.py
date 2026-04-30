52b202d78aef76bec22d795f89bd2ff92e85aea3
Lutain-Brogy <brogylutain@gmail.com>
2026-05-01 00:53:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ if "final_corrections" not in st.session_state:
     st.session_state.final_corrections = []
 
 # 🧠 INPUT GATE
+# Case instructions
 if user_input:
     clean_text = user_input.lower()
 
@@ -40,4 +41,36 @@ if user_input:
 
         else:
             st.write("No case saved yet.")
+
+   # Pains instructions
+
+    elif "new pains" in clean_text:
+        pain_input = st.text_input("Please paste your pains here:")
+
+        if pain_input:
+            import re
+
+            pain_parts = re.split(r"[a-z]\.", pain_input.lower())
+
+            cleaned_pains = []
+
+            for pain in pain_parts:
+                pain = pain.strip()
+
+                if pain:
+                    cleaned_pains.append(pain)
+
+            st.session_state.pains = cleaned_pains
+
+            st.write("Pains saved!")
+
+    elif "my pains" in clean_text:
+        if st.session_state.pains:
+            st.write("Your pains:")
+
+            for i, pain in enumerate(st.session_state.pains):
+                st.write(f"{chr(65+i)}. {pain}")
+
+        else:
+            st.write("No pains saved yet.")
 
