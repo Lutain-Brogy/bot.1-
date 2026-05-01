1b811bb632f0f170f13b8da5b10ec5dabfe580d0
Lutain-Brogy <brogylutain@gmail.com>
2026-05-01 17:33:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,22 +32,21 @@ if "final_corrections" not in st.session_state:
 
 # 🧠 INPUT GATE
 # Case instructions
-
 if user_input:
     clean_text = user_input.lower()
 
-if "new case" in clean_text:
-    new_case = st.text_input("Please paste your case:")
+    if "new case" in clean_text:
+        new_case = st.text_input("Please paste your case:")
 
-    if new_case:
-        st.session_state.case = new_case
-        st.write("Saved!")
+        if new_case:
+            st.session_state.case = new_case
+            st.write("Saved!")
 
-elif "my case" in clean_text:
-    if st.session_state.case:
-        st.write("Your case:", st.session_state.case)
-    else:
-        st.write("No case saved yet.")
+    elif "my case" in clean_text:
+        if st.session_state.case:
+            st.write("Your case:", st.session_state.case)
+        else:
+            st.write("No case saved yet.")
 
 
    # Pains instructions
